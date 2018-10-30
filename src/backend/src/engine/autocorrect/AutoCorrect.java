2ffd78b4a89e019d381706c62ced37beede45599
Haotian Wang <haotian.wang@duke.edu>
2018-10-30 14:23:46 -0400

    Add several set behaviour to correction interface

--- src/backend/src/engine/autocorrect/AutoCorrect.java
+++ src/backend/src/engine/autocorrect/AutoCorrect.java
@@ -1,5 +1,7 @@
 package engine.autocorrect;
 
+import engine.errors.UndefinedKeywordException;
+
 import java.util.List;
 import java.util.Set;
 
@@ -15,6 +17,21 @@ public interface AutoCorrect {
      * @param vocab: A Set of Strings that the interface checks to find the closest String or a list of closest Strings.
      */
     void readSet(Set<String> vocab);
+
+    /**
+     * This add a word to the set of vocabulary of the correction machine.
+     *
+     * @param word: A String to be added.
+     */
+    void addWord(String word);
+
+    /**
+     * This removes a word from the set of vocabulary of the correction machine.
+     *
+     * @param word: A String to be removed.
+     * @throws UndefinedKeywordException
+     */
+    void removeWord(String word) throws UndefinedKeywordException;
 
     /**
      * This gives a closest String to the input String.
